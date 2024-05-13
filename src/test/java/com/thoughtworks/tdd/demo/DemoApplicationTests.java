c20dbdf09dfa293b74886e350c58a3fc4a0629c7
Matteo Vaccari <vaccari@pobox.com>
2024-05-13 21:38:07 +0200

    initial import

---
+++ src/test/java/com/thoughtworks/tdd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.tdd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
