be82cf8e3d69b7e34e0f881728977107c41259f7
Matteo Vaccari <vaccari@pobox.com>
2024-05-21 12:46:29 +0200

    Controller: first test passes (fake it)

--- src/main/java/com/thoughtworks/tdd/cart/domain/Cart.java
+++ src/main/java/com/thoughtworks/tdd/cart/domain/Cart.java
@@ -1,4 +1,7 @@
 package com.thoughtworks.tdd.cart.domain;
 
 public class Cart {
+    public static Cart with(ProductId productId, Quantity quantity) {
+        return null;
+    }
 }
--- src/main/java/com/thoughtworks/tdd/cart/web/AddItemToCartController.java
+++ src/main/java/com/thoughtworks/tdd/cart/web/AddItemToCartController.java
@@ -1,42 +1,33 @@
 package com.thoughtworks.tdd.cart.web;
 
-import com.thoughtworks.tdd.cart.domain.Cart;
-import com.thoughtworks.tdd.cart.domain.CartId;
-import com.thoughtworks.tdd.cart.domain.CartRepository;
+import com.thoughtworks.tdd.cart.domain.AddItemToCartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.ProblemDetailJacksonMixin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 public class AddItemToCartController {
-    private final CartRepository cartRepository;
+    private final AddItemToCartService service;
+
+    public record AddItemToCartRequest(String productId, int quantity) {
+    }
+
+    public record AddItemToCartResponse(List<RequestCartItem> items) {
+        public record RequestCartItem(String productId, int quantity) {}
+    }
 
     @Autowired
-    public AddItemToCartController(CartRepository cartRepository) {
-        this.cartRepository = cartRepository;
+    public AddItemToCartController(AddItemToCartService service) {
+        this.service = service;
     }
 
     @PostMapping("/carts/{cartId}")
     public ResponseEntity<Object> addItemToCart(@PathVariable String cartId, @RequestBody Object request) {
-        Optional<Cart> cart = cartRepository.findCart(new CartId(cartId));
-        if (cart.isEmpty()) {
-            return new ResponseEntity<>(
-                    ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "Cart not found"),
-                    HttpStatus.NOT_FOUND);
-        }
-        return ResponseEntity.ok(Map.of("items", List.of(
-                Map.of("productId", "P4567", "quantity", 1)
-        )));
+        return ResponseEntity.ok(new AddItemToCartResponse(List.of(new AddItemToCartResponse.RequestCartItem("P456", 2))));
     }
 }
