bd84b859c194f62b50a839789c3dc3603edc3d3d
Matteo Vaccari <vaccari@pobox.com>
2024-05-31 17:24:58 +0200

    First service test passes

--- src/test/java/com/thoughtworks/tdd/cart/service/AddItemToCartService.java
+++ src/test/java/com/thoughtworks/tdd/cart/service/AddItemToCartService.java
@@ -1,17 +1,22 @@
 package com.thoughtworks.tdd.cart.service;
 
-import com.thoughtworks.tdd.cart.domain.AddsItemToCart;
-import com.thoughtworks.tdd.cart.domain.CartId;
-import com.thoughtworks.tdd.cart.domain.ProductId;
-import com.thoughtworks.tdd.cart.domain.Quantity;
+import com.thoughtworks.tdd.cart.domain.*;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class AddItemToCartService implements AddsItemToCart {
+    private final CartRepository repository;
+
+    public AddItemToCartService(CartRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Map<ProductId, Quantity> addItemToCart(CartId cartId, ProductId productId, Quantity quantity) {
-
-        return new HashMap<>();
+        Optional<Cart> optionalCart = repository.findCart(cartId);
+        Cart cart = optionalCart.get();
+        cart.addItem(productId, quantity);
+        return cart.items();
     }
 }
--- src/test/java/com/thoughtworks/tdd/cart/service/AddItemToCartServiceTest.java
+++ src/test/java/com/thoughtworks/tdd/cart/service/AddItemToCartServiceTest.java
@@ -5,30 +5,28 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.AbstractMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Disabled("WIP")
 class AddItemToCartServiceTest {
 
     CartRepository repository = mock(CartRepository.class);
-    AddItemToCartService service = new AddItemToCartService();
+    AddItemToCartService service = new AddItemToCartService(repository);
 
     @Test
     void cartFound() {
-        CartId cartId = new CartId("C123");
         Cart cart = new Cart();
-        ProductId product = new ProductId("P111");
         Quantity quantity = new Quantity(2);
-        when(repository.findCart(cartId)).thenReturn(Optional.of(cart));
+        when(repository.findCart(new CartId("C123"))).thenReturn(Optional.of(cart));
 
-        service.addItemToCart(cartId, product, quantity);
+        var map = service.addItemToCart(new CartId("C123"), new ProductId("P111"), new Quantity(2));
 
-        assertThat(cart.items()).containsExactly(
-                new AbstractMap.SimpleEntry<>(product, quantity)
+        assertThat(map).containsExactly(
+                new AbstractMap.SimpleEntry<>(new ProductId("P111"), quantity)
         );
     }
 }
