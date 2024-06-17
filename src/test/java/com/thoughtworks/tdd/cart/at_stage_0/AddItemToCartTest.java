fe829ff8511c01ab88b60c7634c6ff0c87541791
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-06-17 11:15:03 +0200

    Now running the (wrong) test and getting 404

--- src/test/java/com/thoughtworks/tdd/cart/at_stage_0/AddItemToCartTest.java
+++ src/test/java/com/thoughtworks/tdd/cart/at_stage_0/AddItemToCartTest.java
@@ -1,42 +1,23 @@
 package com.thoughtworks.tdd.cart.at_stage_0;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.tdd.cart.CartApplication;
-import com.thoughtworks.tdd.cart.domain.Cart;
-import com.thoughtworks.tdd.cart.domain.CartId;
-import com.thoughtworks.tdd.cart.domain.CartRepository;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@Disabled("feature is WIP")
-@Tag("acceptance")
+//@Disabled("feature is WIP")
+//@Tag("acceptance")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = CartApplication.class)
 public class AddItemToCartTest {
     @Autowired
     TestRestTemplate restTemplate;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     @Test
-    void add_one_item_to_empty_cart() throws JsonProcessingException {
+    void add_one_item_to_empty_cart() {
         var request = """
                 {
                     "productId": "P456",
@@ -44,7 +25,7 @@ public class AddItemToCartTest {
                 }
                 """;
 
-        var responseEntity = restTemplate.postForEntity("/carts/C123", toObject(request), String.class);
+        var responseEntity = restTemplate.postForEntity("/carts/C123", request, String.class);
 
         var expectedResponse = """
                 {
@@ -55,10 +36,6 @@ public class AddItemToCartTest {
                 """;
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isNotNull();
-        assertThat(toObject(responseEntity.getBody())).isEqualTo(toObject(expectedResponse));
-    }
-
-    public Object toObject(String json) throws JsonProcessingException {
-        return objectMapper.readValue(json, Object.class);
+        assertThat(responseEntity.getBody()).isEqualTo(expectedResponse);
     }
 }
