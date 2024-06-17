ad06f0a9da9a05d368e7662f6b997d8a81d8777c
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-06-17 12:17:43 +0200

    Now test passes

---
+++ src/test/java/com/thoughtworks/tdd/cart/at_stage_0/AddItemToCartTest.java
@@ -0,0 +1,48 @@
+package com.thoughtworks.tdd.cart.at_stage_0;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.tdd.cart.CartApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@Disabled("feature is WIP")
+//@Tag("acceptance")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = CartApplication.class)
+public class AddItemToCartTest {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void add_one_item_to_empty_cart() throws JsonProcessingException {
+        String request = """
+                {
+                    "productId": "P456",
+                    "quantity": 1
+                }
+                """;
+
+        Object requestAsObject = objectMapper.readValue(request, Object.class);
+        var responseEntity = restTemplate.postForEntity("/carts/C123", requestAsObject, Object.class);
+
+        var expectedResponse = """
+                {
+                    "items": [
+                        {"productId": "P456", "quantity": 1}
+                    ]
+                }
+                """;
+        Object expectedResponseAsObject = objectMapper.readValue(expectedResponse, Object.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).describedAs("body is null").isNotNull();
+        assertThat(responseEntity.getBody()).isEqualTo(expectedResponseAsObject);
+    }
+}
