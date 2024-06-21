8467dba2f4ef5ed335b0fcff748ab216b894d820
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-06-21 17:19:21 +0200

    First controller test works, but is faking the response

--- src/main/java/com/thoughtworks/tdd/cart/web/AddItemToCartController.java
+++ src/main/java/com/thoughtworks/tdd/cart/web/AddItemToCartController.java
@@ -1,5 +1,10 @@
 package com.thoughtworks.tdd.cart.web;
 
+import com.thoughtworks.tdd.cart.domain.Cart;
+import com.thoughtworks.tdd.cart.domain.CartId;
+import com.thoughtworks.tdd.cart.domain.ProductId;
+import com.thoughtworks.tdd.cart.domain.Quantity;
+import com.thoughtworks.tdd.cart.service.AddItemToCartService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,17 +12,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class AddItemToCartController {
+    private final AddItemToCartService addItemToCartService;
 
-    // This is a fake implementation of the controller that we use to verify
-    // that the first acceptance test can pass
-    @PostMapping("/carts/{cartId}")
-    public ResponseEntity<Object> addItemToCart(@PathVariable String cartId, @RequestBody Object request) {
-        Object body = Map.of("items",
-                List.of(Map.of("productId", "P456", "quantity", 1)));
-        return ResponseEntity.ok(body);
+    public AddItemToCartController(AddItemToCartService service) {
+        this.addItemToCartService = service;
     }
+
+    public record Request(int quantity, String productId) {
+    }
+
+    public record Response(List<Pair<Integer, String>> items) {
+        public static Response from(Cart cart) {
+            return new Response(List.of(
+                    Pair.of(2, "P222"),
+                    Pair.of(3, "P333")
+            ));
+        }
+    }
+
+    @PostMapping("/carts/{cartId}")
+    public ResponseEntity<Response> addItemToCart(@PathVariable String cartId, @RequestBody Request request) {
+        Cart cart = addItemToCartService.addItemToCart(CartId.of(cartId), Quantity.of(request.quantity), ProductId.of(request.productId));
+        Response response = Response.from(cart);
+        return ResponseEntity.ok(response);
+    }
+
 }
---
+++ src/test/java/com/thoughtworks/tdd/cart/web/AddItemToCartControllerTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.tdd.cart.web;
+
+import com.thoughtworks.tdd.cart.domain.Cart;
+import com.thoughtworks.tdd.cart.domain.CartId;
+import com.thoughtworks.tdd.cart.domain.ProductId;
+import com.thoughtworks.tdd.cart.domain.Quantity;
+import com.thoughtworks.tdd.cart.service.AddItemToCartService;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AddItemToCartControllerTest {
+
+    @Test
+    void addsItemToCart() {
+        var service = mock(AddItemToCartService.class);
+        var controller = new AddItemToCartController(service);
+        when(service.addItemToCart(CartId.of("C123"), Quantity.of(3), ProductId.of("P333")))
+                .thenReturn(new Cart()
+                        .add(Quantity.of(2), ProductId.of("P222"))
+                        .add(Quantity.of(3), ProductId.of("P333"))
+                );
+
+        ResponseEntity<AddItemToCartController.Response> result =
+                controller.addItemToCart("C123", new AddItemToCartController.Request(3, "P333"));
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody()).isEqualTo(
+                new AddItemToCartController.Response(
+                        List.of(Pair.of(2, "P222"),
+                                Pair.of(3, "P333"))
+                ));
+    }
+}
